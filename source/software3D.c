debe6776d13bac4a4d08a989a999a73272df14b0
lclMetal <gameslcl@gmail.com>
2019-05-31 22:31:50 +0300

    Add model loading from .obj files

--- source/software3D.c
+++ source/software3D.c
@@ -14,19 +14,33 @@ typedef struct FaceStruct
     short a;
     short b;
     short c;
+    short normal;
 }Face;
 
 typedef struct MeshStruct
 {
     char name[256];
+
     int vertexCount;
     Vector3* vertices;
+
     int faceCount;
     Face* faces;
+
+    int normalCount;
+    Vector3* normals;
+
     Vector3 position;
     Vector3 rotation;
     Matrix4x4 orientation;
 }Mesh;
+
+typedef struct MeshFileStruct
+{
+    int vertexCount;
+    int faceCount;
+    int normalCount;
+}MeshFile;
 
 typedef struct ScreenStruct
 {
@@ -40,8 +54,9 @@ enum flagsEnum
     ROTATE_X           = (1 << 1),
     ROTATE_Y           = (1 << 2),
     ROTATE_Z           = (1 << 3),
-    ROTATE_SINGLE_ONLY = (1 << 4)
-}flags = 0;
+    ROTATE_SINGLE_ONLY = (1 << 4),
+    ENABLE_AXES        = (1 << 5)
+}flags = ENABLE_AXES;
 
 Mesh *cube;
 Mesh *coords;
@@ -52,10 +67,15 @@ short mode = 0;
 
 Screen createScreen(short width, short height);
 Face createFace(short a, short b, short c);
+Face createFaceWithNormal(short a, short b, short c, short normal);
 void drawPointOnScreen(Screen *ptr, Point2D point);
-Mesh *newMesh(char meshName[256], int vertexCount, int faceCount);
+Mesh *newMesh(char meshName[256], int vertexCount, int faceCount, int normalCount);
+void fseekEndOfLine(FILE *f);
+MeshFile getMeshFileInfo(char fileName[256]);
+Mesh *readMeshFromFile(char fileName[256]);
 int setMeshVertex(Mesh *mesh, int vertexNum, Vector3 vertex);
 int setMeshFace(Mesh *mesh, int faceNum, Face face);
+int setMeshNormal(Mesh *mesh, int normalNum, Vector3 normal);
 void setMeshOrientation(Mesh *mesh, Vector3 orientation);
 void renderMesh(Screen *screen, Camera *camera, Mesh *mesh);
 void destroyMesh(Mesh *mesh);
@@ -77,6 +97,19 @@ Face createFace(short a, short b, short c)
     face.a = a;
     face.b = b;
     face.c = c;
+    face.normal = 0;
+
+    return face;
+}
+
+Face createFaceWithNormal(short a, short b, short c, short normal)
+{
+    Face face;
+
+    face.a = a;
+    face.b = b;
+    face.c = c;
+    face.normal = normal;
 
     return face;
 }
@@ -96,7 +129,7 @@ void drawPointOnScreen(Screen *screen, Point2D point)
     }
 }
 
-Mesh *newMesh(char meshName[256], int vertexCount, int faceCount)
+Mesh *newMesh(char meshName[256], int vertexCount, int faceCount, int normalCount)
 {
     Mesh *ptr = NULL;
 
@@ -123,6 +156,17 @@ Mesh *newMesh(char meshName[256], int vertexCount, int faceCount)
         return NULL;
     }
 
+    ptr->normalCount = normalCount;
+    ptr->normals = malloc(sizeof *(ptr->normals) * ptr->normalCount);
+
+    if (!ptr->normals)
+    {
+        free(ptr->vertices);
+        free(ptr->faces);
+        free(ptr);
+        return NULL;
+    }
+
     ptr->position = createVector3(0.0, 0.0, 0.0);
     ptr->rotation = createVector3(0.0, 0.0, 0.0);
     ptr->orientation = createTranslationMatrix(0.0, 0.0, 0.0);
@@ -130,6 +174,161 @@ Mesh *newMesh(char meshName[256], int vertexCount, int faceCount)
     strcpy(ptr->name, meshName);
 
     return ptr;
+}
+
+void fseekEndOfLine(FILE *f)
+{
+    if (f)
+    {
+        char c;
+
+        while ((c = fgetc(f)) != '\n')
+        {
+            if (feof(f))
+                break;
+        }
+    }
+}
+
+MeshFile getMeshFileInfo(char fileName[256])
+{
+    MeshFile mf; // it's mf for MeshFile, you barbaric ogre >:(
+    FILE *f = fopen(fileName, "r+");
+
+    mf.vertexCount =
+    mf.faceCount   =
+    mf.normalCount = 0;
+
+    if (f)
+    {
+        while (1)
+        {
+            char c = fgetc(f);
+
+            if (feof(f)) break;
+
+            switch (c)
+            {
+                case 'v': // this line is either a vertex or a normal
+                    c = fgetc(f);
+
+                    switch (c)
+                    {
+                        // only 'v' --> this is a vertex
+                        case ' ': mf.vertexCount ++;
+                            break;
+
+                        // 'vn' --> this is a normal
+                        case 'n': mf.normalCount ++;
+                            break;
+                    }
+                    break;
+
+                // 'f' --> this is a face
+                case 'f': mf.faceCount ++;
+                    break;
+
+                // end-of-line, nothing to see here
+                case '\n':
+                    break;
+
+                // any other character --> ignore, search for next line
+                default:
+                    fseekEndOfLine(f);
+                    break;
+            }
+        }
+
+        fclose(f);
+    }
+
+    return mf;
+}
+
+Mesh *readMeshFromFile(char fileName[256])
+{
+    Mesh *mesh = NULL;
+    FILE *f = NULL;
+    MeshFile mf;
+
+    mf = getMeshFileInfo(fileName);
+
+    if (!mf.vertexCount)
+        return NULL;
+
+    f = fopen(fileName, "r+");
+
+    if (!f)
+        return NULL;
+
+    mesh = newMesh(fileName, mf.vertexCount + 1, mf.faceCount + 1 , mf.normalCount + 1);
+
+    if (!mesh)
+        return NULL;
+
+    if (f)
+    {
+        Vector3 vec = createVector3(0.0, 0.0, 0.0);
+        Face face;
+        int vertexNum = 0, faceNum = 0, normalNum = 0;
+
+        while (1)
+        {
+            char c = fgetc(f), d;
+
+            if (feof(f))
+                break;
+
+            switch (c)
+            {
+                case 'v': // this line is either a vertex or a normal
+                    d = fgetc(f);
+
+                    switch (d)
+                    {
+                        // only 'v' --> this is a vertex
+                        case ' ':
+                            fscanf(f, "%lf %lf %lf",
+                                    &vec.x, &vec.y, &vec.z);
+                            setMeshVertex(mesh, vertexNum++, vec);
+                            break;
+
+                        // 'vn' --> this is a normal
+                        case 'n':
+                            fscanf(f, " %lf %lf %lf",
+                                    &vec.x, &vec.y, &vec.z);
+                            setMeshNormal(mesh, normalNum++, vec);
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                // 'f' --> this is a face
+                case 'f':
+                    fscanf(f, "%hd//%hd %hd//%hd %hd//%hd",
+                            &face.a, &face.normal,
+                            &face.b, &face.normal,
+                            &face.c, &face.normal);
+                    setMeshFace(mesh, faceNum++, createFaceWithNormal(face.a - 1, face.b - 1, face.c - 1, face.normal - 1));
+                    break;
+
+                // end-of-line, nothing to see here
+                case '\n':
+                    break;
+
+                // any other character --> ignore, search for next line
+                default:
+                    fseekEndOfLine(f);
+                    break;
+            }
+        }
+
+        fclose(f);
+    }
+
+    return mesh;
 }
 
 int setMeshVertex(Mesh *mesh, int vertexNum, Vector3 vertex)
@@ -148,6 +347,16 @@ int setMeshFace(Mesh *mesh, int faceNum, Face face)
     if (faceNum < 0 || faceNum >= mesh->faceCount) return -2;
 
     mesh->faces[faceNum] = face;
+
+    return 0;
+}
+
+int setMeshNormal(Mesh *mesh, int normalNum, Vector3 normal)
+{
+    if (!mesh) return -1;
+    if (normalNum < 0 || normalNum >= mesh->normalCount) return -2;
+
+    mesh->normals[normalNum] = normal;
 
     return 0;
 }
@@ -193,16 +402,22 @@ void renderMesh(Screen *screen, Camera *camera, Mesh *mesh)
         pointB = project(screen->width, screen->height, vertexB, transformMatrix);
         pointC = project(screen->width, screen->height, vertexC, transformMatrix);
 
-        setpen(255, 0, 255, 0, 7);
-        drawPointOnScreen(screen, pointA);
-        drawPointOnScreen(screen, pointB);
-        drawPointOnScreen(screen, pointC);
+        if (mode)
+        {
+            setpen(255, 255, 255, 0, 1);
+            moveto(pointA.x, pointA.y);
+            lineto(pointB.x, pointB.y);
+            lineto(pointC.x, pointC.y);
+            lineto(pointA.x, pointA.y);
+        }
 
-        setpen(255, 255, 255, 0, 1);
-        moveto(pointA.x, pointA.y);
-        lineto(pointB.x, pointB.y);
-        lineto(pointC.x, pointC.y);
-        lineto(pointA.x, pointA.y);
+        if (mode <= 1)
+        {
+            setpen(255, 0, 255, 0, 7);
+            drawPointOnScreen(screen, pointA);
+            drawPointOnScreen(screen, pointB);
+            drawPointOnScreen(screen, pointC);
+        }
     }
 
     /*for (i = 0; i < mesh->vertexCount; i ++)
